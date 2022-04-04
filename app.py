4793ba24b3e63851856c49b467a97a7619b4ec00
sridharandharani <sridharandharani1815@gmail.com>
2022-04-04 10:13:16 +0530

    website

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def Welcome():
     return "Welcome to my website"
 
+@app.route('/contact')
+def Contact_page():
+    return "Contact page"
+
+@app.route('/home')
+def Home_page():
+    return "Home page"
+
+@app.route('/gallery')
+def Gallery_page():
+    return "Gallery page"
+
 if __name__ == "__main__":
     app.run()
